a68bfd816113175dc9e0986e9504d384a10fb4c3
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-04-16 05:27:26 -0500

    se agregó test para función filterMd

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -4,7 +4,9 @@
 //import {funciones que se importan} from 'path';
 const mdLinks = require('../src/md-links.js');
 const pathFile = '/home/elenasilvana/Documents/movies.txt';
-const pathDirectory = '/home/elenasilvana/Documentses un directorio';
+const pathDirectory = '/home/elenasilvana/Documents';
+//multiples archivos md en un directorio
+const pathMdDir = '/home/elenasilvana/Documents/Laboratoria/Proyectos/2/GDL002-data-lovers'; 
 
 
 describe('mdLinks', () => {
@@ -13,6 +15,12 @@ describe('mdLinks', () => {
   	expect(typeof mdLinks).toBe('object');
     console.log('is an object');
   });
+
+
+});
+
+
+describe('fileOrDirectory', () => {
 
 //test fileOrDirectory
   it('fileOrDirectory should be a function', () => {
@@ -25,7 +33,6 @@ describe('mdLinks', () => {
   	mdLinks.fileOrDirectory(pathFile, function(result){
   		expect(result).toBe('es un archivo');
   	});
-
   });
 //test if path is a directory
 	it('fileOrDirectory should say if path go to a directory', () => {
@@ -34,5 +41,28 @@ describe('mdLinks', () => {
 			expect(result).toBe('es un directorio');
 		});
 	});
- // it('mdLinks returns', () => { expect(mdLinks('aquielpathabsoluto')).toEqual(['un path absoluto']);
+
+
+})
+
+
+//function for reading directory and filter .md files
+describe('filterMd', () => {
+
+	it('should be a function', () => {
+
+		expect(typeof mdLinks.filterMd).toBe('function');
+	});
+
+	it('filterMd should filter .md files in a directory', () => {
+		//debería retornar un array con los resultados de los archivos encontrados
+		expect(mdLinks.filterMd(pathMdDir, function(result){
+			expect(result.length).toBe(3);
+			/* debería tener un archivo md que encontrar */
+			})
+		);
+	});
+
 });
+
+//function for open and reading file
