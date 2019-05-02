d8f7b4081debb3a9a3123265cd7c95e8d7dc2c98
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-05-01 22:24:54 -0500

    se encadenaron validaciones y llamada de funciones en callbacks

--- src/index.js
+++ src/index.js
@@ -4,8 +4,29 @@ const mdLinks = require('./md-links.js');
 //arguments from the CLI, path to check file 
 const path = process.argv[2];
 
-mdLinks.fileOrDirectory(path, (result)=>{console.log(result)});
+if(!mdLinks.validatePath(path)){
+	process.exit(2);
+}
+
+mdLinks.fileOrDirectory(path, (result)=>{
+	
+	if(result === 'directorio'){
+		mdLinks.filterMd(path, (result)=>{
+			const route = `${path}\/${result[0]}`;
+			mdLinks.readAndGetLinks(route, (res)=>{
+				console.log(res);
+			});			
+		});
+	}
+	else if(result === 'archivo'){
+		mdLinks.readAndGetLinks(path, (result)=>{
+			console.log(result);
+		});
+	}
+
+
+});
 
 //ruta filterMd  /home/elenasilvana/Documents/Laboratoria/Proyectos/2/GDL002-data-lovers
 
-mdLinks.filterMd(path, (result)=> {console.log(result)});	
+//mdLinks.filterMd(path, (result)=> {console.log(result)});	
